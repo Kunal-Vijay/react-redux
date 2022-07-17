18ebbc721539dc1f6ce347b8d5c2196d3978a4c3
Kunal <kunal20vijay00@gmail.com>
2022-07-17 19:01:33 +0530

    Reducer Function

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function buyCake() {
   };
 }
 
+// Setting up initial state
+const initialState = {
+  numOfCakes: 10,
+};
 
+// reducer function - (prevState,action) => nextState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
